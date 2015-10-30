1077029b518532a3a614528165570a8fa64283db
Dylan McDonald <dmcdonald21@cnm.edu>
2015-10-30 10:09:11 -0600

    Added introduction

--- public_html/index.php
+++ public_html/index.php
@@ -27,6 +27,24 @@ $ANGULAR_VERSION = "1.4.7";
 			<h1>Misquotes</h1>
 			<div class="row">
 				<div class="col-md-4">
+					<img class="img-responsive" src="images/miss-quote.png" alt="">
+				</div>
+				<div class="col-md-8">
+					<blockquote>
+						That is the problem with memes. If you have the right font and the right photo, any quote can seem real. And I'll tell you how I know that. Because for years now you may have seen multiple photos of me comparing gun control to airport security. It's an interesting thought. Here's the thing: I never said that! Even though, I've now seen it so many times now I'm starting to genuinely wonder if I ever did.
+						<footer>
+							John Oliver on <cite>Last Week Tonight</cite>
+						</footer>
+					</blockquote>
+					<p>
+						I now present you with the opportunity to make your own misquotes. Want to claim you solved <var>P</var> = <var>NP</var>? You've come to the right place. Want to prove Sir Isaac Newton was hit on the head with an apple while making apple pie? Done!
+					</p>
+					<h3>Enjoy!</h3>
+				</div>
+			</div>
+			<hr />
+			<div class="row">
+				<div class="col-md-4">
 					<form name="addMisquoteForm" id="addMisquoteForm" class="form-horizontal well" ng-submit="createMisquote(newMisquote, addMisquoteForm.$valid);" ng-hide="isEditing" novalidate>
 						<h2>Create Misquote</h2>
 						<hr />
