6010856925594b5cba6c06b9374c014ba778e0dc
Dylan McDonald <dmcdonald21@cnm.edu>
2016-02-28 14:06:18 -0700

    Created a template for Angular Route

---
+++ public_html/angular/templates/misquote.php
@@ -0,0 +1,9 @@
+<div>
+	<h1>View Misquote {{ misquote.misquoteId }}</h1>
+	<blockquote>
+		<p>{{ misquote.misquote }}</p>
+		<footer>
+			{{ misquote.attribution }} <cite>added by {{ misquote.submitter }}</cite>
+		</footer>
+	</blockquote>
+</div>
