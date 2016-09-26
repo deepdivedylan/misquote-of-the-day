838a07077ebc8b43b66ddd898b1967cee90efccf
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-26 11:37:57 -0600

    added master app template

---
+++ webpack/index.php
@@ -0,0 +1,22 @@
+<?php
+// start session and enable XSRF protection
+require_once(dirname(__DIR__) . "/lib/xsrf.php");
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+setXsrfCookie();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+
+		<title>Misquote of the Day</title>
+	</head>
+	<body>
+		<misquote-app>Loading&hellip;</misquote-app>
+	</body>
+</html>
