caaa396dbedb8aa62b52c6184247e7486bc2c7c5
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-26 18:37:07 -0600

    added app module

---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import {MisquoteService} from "./services/misquote-service";
+
+const moduleDeclarations = [AppComponent];
+
+@NgModule({
+	imports:      [BrowserModule, FormsModule, HttpModule, routing],
+	declarations: [...moduleDeclarations, ...allAppComponents],
+	bootstrap:    [AppComponent],
+	providers:    [appRoutingProviders, MisquoteService]
+})
+export class AppModule {}
