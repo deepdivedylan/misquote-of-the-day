935dbce3283ccfe694f4de1de420b4a2a58a730e
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-26 23:50:48 -0600

    converted edit form to an Angular 2 form

--- public_html/templates/misquote.php
+++ public_html/templates/misquote.php
@@ -5,3 +5,49 @@
 		{{ misquote.attribution }} <cite>added by {{ misquote.submitter }}</cite>
 	</footer>
 </blockquote>
+<form #misquoteForm="ngForm" name="misquoteForm" id="misquoteForm" class="form-horizontal well" novalidate>
+	<h2>Edit Misquote</h2>
+	<hr />
+	<div class="form-group" [ngClass]="{ 'has-error': misquote.touched && misquote.invalid }">
+		<label for="misquote">Misquote</label>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-comment" aria-hidden="true"></i>
+			</div>
+			<input type="text" name="misquote" id="misquote" class="form-control" maxlength="255" required [(ngModel)]="misquote.misquote" #misquoteText="ngModel" />
+		</div>
+		<div [hidden]="misquoteText.valid || misquoteText.pristine" class="alert alert-danger" role="alert">
+			<p *ngIf="misquoteText.errors?.required">Misquote is required.</p>
+			<p *ngIf="misquoteText.errors?.maxlength">Misquote is too long.</p>
+		</div>
+	</div>
+	<div class="form-group" [ngClass]="{ 'has-error': attribution.touched && attribution.invalid }">
+		<label for="attribution">Attribution</label>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-quote-left" aria-hidden="true"></i>
+			</div>
+			<input type="text" name="attribution" id="attribution" class="form-control" maxlength="64" required [(ngModel)]="misquote.attribution" #attribution="ngModel" />
+		</div>
+		<div [hidden]="attribution.valid || attribution.pristine" class="alert alert-danger" role="alert">
+			<p *ngIf="attribution.errors?.required">Attribution is required.</p>
+			<p *ngIf="attribution.errors?.maxlength">Attribution is too long.</p>
+		</div>
+	</div>
+	<div class="form-group" [ngClass]="{ 'has-error': submitter.touched && submitter.invalid }">
+		<label for="submitter">Submitter</label>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-user" aria-hidden="true"></i>
+			</div>
+			<input type="text" name="submitter" id="submitter" class="form-control" maxlength="64" required [(ngModel)]="misquote.submitter" #submitter="ngModel" />
+		</div>
+		<div [hidden]="submitter.valid || submitter.pristine" class="alert alert-danger" role="alert">
+			<p *ngIf="submitter.errors?.required">Submitter is required.</p>
+			<p *ngIf="submitter.errors?.maxlength">Submitter is too long.</p>
+		</div>
+	</div>
+	<button type="submit" class="btn btn-info btn-lg" [disabled]="misquoteForm.invalid"><i class="fa fa-share"></i> Misquote</button>
+	<button type="reset" class="btn btn-warning btn-lg"><i class="fa fa-ban"></i> Cancel</button>
+	<button type="button" class="btn btn-danger btn-lg" (click)="deleteMisquote(misquote.misquoteId);"><i class="fa fa-trash"></i> Delete Misquote</button>
+</form>
