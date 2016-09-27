60895e0e50b57c5fd7364302b3d30ddf91891841
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-26 22:37:17 -0600

    created a table for the list component

--- public_html/templates/misquote-list.php
+++ public_html/templates/misquote-list.php
@@ -1,1 +1,10 @@
 <h1>Misquote List Component</h1>
+<table class="table table-bordered table-hover table-responsive table-striped table-word-wrap">
+	<tr><th>Misquote ID</th><th>Misquote</th><th>Attribution</th><th>Submitter</th></tr>
+	<tr *ngFor="let misquote of misquotes">
+		<td>{{ misquote.misquoteId }}</td>
+		<td>{{ misquote.misquote }}</td>
+		<td>{{ misquote.attribution }}</td>
+		<td>{{ misquote.submitter }}</td>
+	</tr>
+</table>
