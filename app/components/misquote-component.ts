dc5b9fa97a65bc1d7b2ebf31da645aaff3ba8892
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-26 20:49:04 -0600

    added routing parameters

--- app/components/misquote-component.ts
+++ app/components/misquote-component.ts
@@ -1,7 +1,20 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {MisquoteService} from "../services/misquote-service";
+import {Misquote} from "../classes/misquote";
 
 @Component({
 	templateUrl: "./templates/misquote.php"
 })
 
-export class MisquoteComponent {}
+export class MisquoteComponent implements OnInit {
+	misquote: Misquote;
+
+	constructor(private misquoteService: MisquoteService,  private route: ActivatedRoute) {}
+
+	ngOnInit() : void {
+		this.route.params.forEach((params : Params) => {
+			let misquoteId = +params["misquoteId"];
+		});
+	}
+}
