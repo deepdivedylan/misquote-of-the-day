af358a7c56adae77194a520dcc35156edc511aa1
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-26 21:19:17 -0600

    added loading of all misquotes

--- app/components/misquote-list-component.ts
+++ app/components/misquote-list-component.ts
@@ -1,7 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {MisquoteService} from "../services/misquote-service";
+import {Misquote} from "../classes/misquote";
 
 @Component({
 	templateUrl: "./templates/misquote-list.php"
 })
 
-export class MisquoteListComponent {}
+export class MisquoteListComponent implements OnInit {
+	misquotes: Misquote[] = [];
+
+	constructor(private misquoteService: MisquoteService) {}
+
+	ngOnInit() : void {
+		this.misquoteService.getAllMisquotes()
+			.subscribe(misquotes => this.misquotes = misquotes);
+	}
+}
