612178e4baf43bf058a04f8f982d35c655fa13b9
Dylan McDonald <dmcdonald21@cnm.edu>
2016-09-29 15:48:10 -0600

    added introduction

--- public_html/templates/misquote-app.php
+++ public_html/templates/misquote-app.php
@@ -1,3 +1,27 @@
 <main class="container">
+	<h1>Misquotes</h1>
+	<div class="row">
+		<div class="col-md-4">
+			<img class="img-responsive" src="images/miss-quote.png" alt="">
+		</div>
+		<div class="col-md-8">
+			<blockquote>
+				That is the problem with memes. If you have the right font and the right photo, any quote can seem real.
+				And I'll tell you how I know that. Because for years now you may have seen multiple photos of me
+				comparing gun control to airport security. It's an interesting thought. Here's the thing: I never said
+				that! Even though, I've now seen it so many times now I'm starting to genuinely wonder if I ever did.
+				<footer>
+					John Oliver on <cite>Last Week Tonight</cite>
+				</footer>
+			</blockquote>
+			<p>
+				I now present you with the opportunity to make your own misquotes. Want to claim you solved <var>P</var>
+				= <var>NP</var>? You've come to the right place. Want to prove Sir Isaac Newton was hit on the head with
+				an apple while making apple pie? Done!
+			</p>
+			<h3>Enjoy!</h3>
+		</div>
+	</div>
+	<hr/>
 	<router-outlet></router-outlet>
 </main>
