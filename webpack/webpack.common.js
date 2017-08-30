439978a71967776224c8c650f2232db09faaf913
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-30 10:49:51 -0600

    upgraded webpack configuration

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,64 +1,56 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var helpers = require('./helpers');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var helpers = require("./helpers");
 
 module.exports = {
 	entry: {
-		'polyfills': helpers.root('app') + '/polyfills.ts',
-		'vendor': helpers.root('app') + '/vendor.ts',
-		'app': helpers.root('app') + '/app.ts',
-		'css': helpers.root('app') + '/app.css'
+		"polyfills": helpers.root("app") + "/polyfills.ts",
+		"vendor": helpers.root("app") + "/vendor.ts",
+		"app": helpers.root("app") + "/app.ts",
+		"css": helpers.root("app") + "/app.css"
 	},
 
 	resolve: {
-		extensions: ['', '.js', '.ts']
+		extensions: [".ts", ".js"]
 	},
 
 	module: {
-		loaders: [
+		rules: [
 			{
-				test: /\.html$/,
-				loader: 'html'
+				test: /\.(html|php)$/,
+				loader: "html-loader"
 			},
 			{
 				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-				loader: 'file?name=assets/[name].[hash].[ext]'
-			},
-			{
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "url-loader?name=/[name].[hash].[ext]&limit=10000&mimetype=application/font-woff"
-			},
-			{
-				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: "file-loader?name=/[name].[hash].[ext]"
+				loader: "file-loader?name=/assets/[name].[hash].[ext]"
 			},
 			{
 				test: /\.css$/,
-				exclude: helpers.root('src', 'app'),
-				loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-			},
-			{
-				test: /\.css$/,
-				include: helpers.root('src', 'app'),
-				loader: 'raw'
+				loader: ExtractTextPlugin.extract({ fallbackLoader: "style-loader", loader: "css-loader?minimize=true" })
 			},
 			{
 				test: /\.ts$/,
-				loaders: ['ts', 'angular2-template-loader']
+				loaders: ["awesome-typescript-loader"]
 			}
 		]
 	},
 
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin({
-			name: ['app', 'vendor', 'polyfills']
+			name: ["app", "vendor", "polyfills"]
+		}),
+
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			"window.jQuery": "jquery"
 		}),
 
 		new HtmlWebpackPlugin({
-			inject: 'head',
-			filename: helpers.root('public_html') + '/index.php',
-			template: helpers.root('webpack') + '/index.php'
+			inject: "head",
+			filename: helpers.root("public_html") + "/index.php",
+			template: helpers.root("webpack") + "/index.php"
 		})
 	]
 };
--- webpack/webpack.live.js
+++ webpack/webpack.live.js
@@ -1,33 +1,33 @@
-var webpack = require('webpack');
-var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var commonConfig = require('./webpack.common.js');
-var helpers = require('./helpers');
+var webpack = require("webpack");
+var webpackMerge = require("webpack-merge");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var commonConfig = require("./webpack.common.js");
+var helpers = require("./helpers");
 
-const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+const ENV = process.env.NODE_ENV = process.env.ENV = "production";
 
 module.exports = webpackMerge(commonConfig, {
-	devtool: 'source-map',
+	devtool: "source-map",
 
 	output: {
-		path: helpers.root('public_html/dist'),
-		publicPath: 'dist',
-		filename: '[name].[hash].js',
-		chunkFilename: '[id].[hash].chunk.js'
-	},
-
-	htmlLoader: {
-		minimize: false // workaround for ng2
+		path: helpers.root("public_html/dist"),
+		publicPath: "dist",
+		filename: "[name].[hash].js",
+		chunkFilename: "[id].[hash].chunk.js"
 	},
 
 	plugins: [
-		new webpack.NoErrorsPlugin(),
-		new webpack.optimize.DedupePlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.optimize.UglifyJsPlugin(),
-		new ExtractTextPlugin('[name].[hash].css'),
+		new ExtractTextPlugin("[name].[hash].css"),
 		new webpack.DefinePlugin({
-			'process.env': {
-				'ENV': JSON.stringify(ENV)
+			"process.env": {
+				"ENV": JSON.stringify(ENV)
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			htmlLoader: {
+				minimize: false // workaround for ng2
 			}
 		})
 	]
