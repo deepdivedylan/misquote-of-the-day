ed820fc2a8100cb2cc592182de4e8af3614b3d93
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-30 11:24:35 -0600

    disabled live mode

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { enableProdMode } from '@angular/core';
 
-enableProdMode();
+// enableProdMode();
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
