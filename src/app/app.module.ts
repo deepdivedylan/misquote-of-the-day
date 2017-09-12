5abe10176ba2875599c58a6abdc7f7902fecfb17
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-12 09:00:44 -0600

    updated app providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
-import {MisquoteService} from "./services/misquote-service";
 
 const moduleDeclarations = [AppComponent];
 
@@ -12,6 +11,6 @@ const moduleDeclarations = [AppComponent];
 	imports:      [BrowserModule, FormsModule, HttpModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [appRoutingProviders, MisquoteService]
+	providers:    [...appRoutingProviders]
 })
 export class AppModule {}
