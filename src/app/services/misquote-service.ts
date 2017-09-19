91ed6a79398b20f4261fb0e3ba45af6afcd54f0b
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-18 20:16:22 -0600

    deleted base service

--- src/app/services/misquote-service.ts
+++ src/app/services/misquote-service.ts
@@ -1,7 +1,6 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
-import {BaseService} from "./base-service";
 import {Misquote} from "../classes/misquote";
 import {Status} from "../classes/status";
 
