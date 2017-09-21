59224ceaf5e7857519d4e95b4f40969779756b15
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-21 11:12:12 -0600

    updated SQL structure to use UUID

--- sql/misquote.sql
+++ sql/misquote.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS misquote;
+
 CREATE TABLE misquote (
-	misquoteId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	misquoteId BINARY(16) NOT NULL,
 	attribution VARCHAR(64) NOT NULL,
 	misquote VARCHAR(255) NOT NULL,
 	submitter VARCHAR(64) NOT NULL,
