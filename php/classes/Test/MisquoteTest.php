86388ba24ca6f0296a55af503a622e3d186454af
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-21 23:23:07 -0600

    added doc block

--- php/classes/Test/MisquoteTest.php
+++ php/classes/Test/MisquoteTest.php
@@ -9,6 +9,12 @@ require_once(dirname(__DIR__) . "/autoload.php");
 // grab the uuid generator
 require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
+/**
+ * Full PHPUnit test for the Misquote class
+ *
+ * This is a complete PHPUnit test of the Misquote class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ **/
 class MisquoteTest extends MisquoteOfTheDayTest {
 	/**
 	 * attribution of this Misquote
