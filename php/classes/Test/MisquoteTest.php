6d6ec041447d5079a06b6f9fbc2231b35c32ba53
Dylan McDonald <dmcdonald21@cnm.edu>
2017-09-21 22:53:11 -0600

    passed valid delete test

--- php/classes/Test/MisquoteTest.php
+++ php/classes/Test/MisquoteTest.php
@@ -77,4 +77,25 @@ class MisquoteTest extends MisquoteOfTheDayTest {
 		$this->assertEquals($pdoMisquote->getSubmitter(), $this->VALID_SUBMITTER);
 	}
 
+	/**
+	 * test creating a Misquote and then deleting it
+	 **/
+	public function testDeleteValidMisquote() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("misquote");
+
+		// create a Misquote and insert it into mySQL
+		$misquoteId = generateUuidV4();
+		$misquote = new Misquote($misquoteId, $this->VALID_ATTRIBUTION, $this->VALID_MISQUOTE, $this->VALID_SUBMITTER);
+		$misquote->insert($this->getPDO());
+
+		// delete the Misquote from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("misquote"));
+		$misquote->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce the Misquote does not exist
+		$pdoMisquote = Misquote::getMisquoteByMisquoteId($this->getPDO(), $misquoteId);
+		$this->assertNull($pdoMisquote);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("misquote"));
+	}
 }
