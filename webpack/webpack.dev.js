f8995fce7a276ba4a785ff76e3d78dee821ecdd9
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-06 09:41:36 -0700

    defined environment for both dev and live

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,6 +4,8 @@ var commonConfig = require("./webpack.common.js");
 var helpers = require("./helpers");
 var targetUrl = require("./target.js");
 
+const ENV = process.env.NODE_ENV = process.env.ENV = "dev";
+
 module.exports = webpackMerge(commonConfig, {
 	devtool: "cheap-module-eval-source-map",
 
@@ -15,7 +17,12 @@ module.exports = webpackMerge(commonConfig, {
 	},
 
 	plugins: [
-		new ExtractTextPlugin("[name].css")
+		new ExtractTextPlugin("[name].css"),
+		new webpack.DefinePlugin({
+			"process.env": {
+				"ENV": JSON.stringify(ENV)
+			}
+		})
 	],
 
 	devServer: {
--- webpack/webpack.live.js
+++ webpack/webpack.live.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var commonConfig = require("./webpack.common.js");
 var helpers = require("./helpers");
 
-const ENV = process.env.NODE_ENV = process.env.ENV = "production";
+const ENV = process.env.NODE_ENV = process.env.ENV = "live";
 
 module.exports = webpackMerge(commonConfig, {
 	devtool: "source-map",
