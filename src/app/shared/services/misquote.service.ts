b0c252c56e44ce73508bf010c53457176243155b
Dylan McDonald <dmcdonald21@cnm.edu>
2018-11-15 08:50:04 -0800

    moved misquote service to shared directory

--- src/app/shared/services/misquote.service.ts
+++ src/app/shared/services/misquote.service.ts
@@ -1,8 +1,8 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
-import {Misquote} from "../classes/misquote";
-import {Status} from "../classes/status";
+import {Misquote} from "../interfaces/misquote";
+import {Status} from "../interfaces/status";
 
 @Injectable()
 export class MisquoteService {
